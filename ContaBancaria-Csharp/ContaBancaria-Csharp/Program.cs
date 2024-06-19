6970b2d99f9a6013661e4cd2401a5efeabace414
Leonardo Pestille <leonardopestille@gmail.com>
2024-06-19 14:29:49 -0400

    Atualizando Program.cs
    Tela Inicial com Criação da conta.

--- ContaBancaria-Csharp/ContaBancaria-Csharp/Program.cs
+++ ContaBancaria-Csharp/ContaBancaria-Csharp/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Seja bem vindo ao Programa de Gerenciamento de Contas Bancárias DataInsight");
+            Console.Write("Digite o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Conta conta = new Conta(titular, numeroConta);
         }
     }
 }
