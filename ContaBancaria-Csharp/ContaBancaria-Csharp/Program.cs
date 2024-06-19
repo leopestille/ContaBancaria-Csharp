f54ed66d4ad6751c6c570fad2dd47213b1be8449
Leonardo Pestille <leonardopestille@gmail.com>
2024-06-19 15:25:12 -0400

    Menu de Opções e Switch de operações
    Criando menu de Opções e switch de operações

--- ContaBancaria-Csharp/ContaBancaria-Csharp/Program.cs
+++ ContaBancaria-Csharp/ContaBancaria-Csharp/Program.cs
@@ -12,6 +12,42 @@
             int numeroConta = int.Parse(Console.ReadLine());
 
             Conta conta = new Conta(titular, numeroConta);
+
+            bool continuar = true;
+            while (continuar) {
+                Console.WriteLine("\nMenu de Opções:");
+                Console.WriteLine("1 - Depositar");
+                Console.WriteLine("2 - Sacar");
+                Console.WriteLine("3 - Consultar Saldo");
+                Console.WriteLine("4 - Sair");
+
+                Console.Write("Digite a opção desejada: ");
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao) {
+                    case 1:
+                        Console.Write("Digite o Valor a ser Depositado: ");
+                        double valorDeposito = double.Parse(Console.ReadLine());
+                        conta.Depositar(valorDeposito);
+                        break;
+                    case 2:
+                        Console.Write("Digite o Valor a ser Sacado: ");
+                        double valorSaque = double.Parse(Console.ReadLine());
+                        conta.Sacar(valorSaque);
+                        break;
+                    case 3:
+                        double saldoAtual = conta.ConsultarSaldo();
+                        Console.WriteLine("Seu saldo atual é de: R$ {0:F2}", saldoAtual);
+                        break;
+                    case 4:
+                        Console.WriteLine("Obrigado por utilizar os nossos serviços. Até mais!");
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção Inválida. Tente novamente.");
+                        break;
+                }
+            }
         }
     }
 }
